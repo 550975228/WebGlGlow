73391498fa58e36e394d53ef9eecbb06523aad95
Huel <Huel.fuchsberger@gmx.de>
2013-03-26 17:19:26 +0100

    Refactored Triangle

--- geometry.js
+++ geometry.js
@@ -1,9 +1,8 @@
 function SquareGeometry()
 {
     this.vertices = [];
-    this.colors = [];
 
-    this.create = function create(width, height, color)
+    this.create = function create(width, height)
          {
              var size = { x: width/2, y: height/2 };
 
@@ -13,11 +12,23 @@ function SquareGeometry()
                   size.x, -size.y,  0.0,
                  -size.x, -size.y,  0.0
                  ];
-
-             this.colors = []
-             for (var i=0; i < 4; i++)
-             {
-                 this.colors = this.colors.concat(color);
-             }
          }
+}
+
+function TriangleGeometry()
+{
+	this.vertices = [];
+	
+	this.create = function create(width, height)
+         {
+             var size = { x: width/2, y: height/2 };
+
+             this.vertices = [
+                  size.x,  size.y,  0.0,
+                  size.x, -size.y,  0.0,
+                 -size.x, -size.y,  0.0
+                 ];
+
+         }
+	
 }
--- objects.js
+++ objects.js
@@ -1,10 +1,44 @@
-var triangleVertexPositionBuffer;
+
+// helper function to create a vertex buffer out of the provided array
+function createVertexBuffer(dataArray, itemSize)
+{
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(dataArray), gl.STATIC_DRAW);
+    vertexBuffer.itemSize = itemSize;
+    vertexBuffer.numItems = dataArray.length / itemSize;
+
+    return vertexBuffer;
+}
+
+// helper function to create a index buffer out of the provided array
+function createIndexBuffer(indicesArray)
+{
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indicesArray), gl.STATIC_DRAW);
+    indexBuffer.itemSize = 1;
+    indexBuffer.numItems = indicesArray.length;
+
+    return indexBuffer;
+}
+
+function Triangle(width, height)
+{
+	//this.Geometry = new TriangleGeometry();
+	//this.Geometry.create(width, height);
+	this.VertexPositionBuffer;
+}
+
+var triangle;
 var squareVertexPositionBuffer;
 
 function initBuffers() 
 {
-    triangleVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	triangle = new Triangle(1,1);
+	
+    triangle.VertexPositionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangle.VertexPositionBuffer);
     var vertices = 
 	[
         0.0,  1.0,  0.0,
@@ -13,8 +47,8 @@ function initBuffers()
     ];
 	
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    triangleVertexPositionBuffer.itemSize = 3;
-    triangleVertexPositionBuffer.numItems = 3;
+    triangle.VertexPositionBuffer.itemSize = 3;
+    triangle.VertexPositionBuffer.numItems = 3;
 
     squareVertexPositionBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, squareVertexPositionBuffer);
@@ -41,10 +75,10 @@ function drawScene()
     mat4.identity(mvMatrix);
 
     mat4.translate(mvMatrix, [-1.5, 0.0, -7.0]);
-    gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangleVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.bindBuffer(gl.ARRAY_BUFFER, triangle.VertexPositionBuffer);
+    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, triangle.VertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
     setMatrixUniforms();
-    gl.drawArrays(gl.TRIANGLES, 0, triangleVertexPositionBuffer.numItems);
+    gl.drawArrays(gl.TRIANGLES, 0, triangle.VertexPositionBuffer.numItems);
 
 
     mat4.translate(mvMatrix, [3.0, 0.0, 0.0]);
